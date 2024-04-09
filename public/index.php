67b3053cbf036e180028a45cf2b6efc5998e6f61
abby209 <abiskepple@gmail.com>
2024-04-08 23:58:55 -0400

    Added files

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Determine if the application is in maintenance mode... 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
@@ -15,3 +15,5 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
+
+
