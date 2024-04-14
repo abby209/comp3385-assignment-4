d1ee3a3ef47211051a2d7b271f6adb8db24b6199
abby209 <abiskepple@gmail.com>
2024-04-14 13:38:56 -0400

    Added files

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    if ($request->user()) {
+        return response()->json($request->user());
+    } else {
+      // Handle unauthenticated user case
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
 })->middleware('auth:sanctum');
+
+Route::prefix('v1/movies')->group(function () {
+    Route::get('/', [MovieController::class, 'index']);  // GET request to /api/v1/movies
+    Route::post('/', [MovieController::class, 'store']); // POST request to /api/v1/movies
+});
